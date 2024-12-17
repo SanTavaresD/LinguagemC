38bce2c0605680d32f97aaa37a8e7abcb7bbcac2
Daniel Tavares <santana.tavares.d@gmail.com>
2024-12-17 20:44:50 -0300

    17/12/2024

--- EstruturaDeDados/ListaEncadeada/Programeseufuturo/insercao_no_inicio_fim_meio.c
+++ EstruturaDeDados/ListaEncadeada/Programeseufuturo/insercao_no_inicio_fim_meio.c
@@ -11,14 +11,25 @@ typedef struct no {
     struct no *proximo;
 }No;
 
+typedef struct {
+    No *inicio;
+    int tam;
+ } Lista;
+
+ void criar_lista(Lista *lista){
+    lista -> inicio = NULL;
+    lista -> tam = 0;
+ }
+
 // Procedimento para inserir no inicio
-void inserir_no_inicio(No **lista, int num){
+void inserir_no_inicio(Lista *lista, int num){
     No *novo = malloc(sizeof(No));
 
     if(novo){
         novo -> valor = num;
-        novo -> proximo = *lista;
-        *lista = novo;
+        novo -> proximo = lista -> inicio;
+        lista -> inicio = novo;
+        lista -> tam++;
     }
     else{
         printf("Erro ao alocar memoria!\n");
@@ -26,7 +37,7 @@ void inserir_no_inicio(No **lista, int num){
 }
 
 // Procedimento para inserir no fim
-void inserir_no_fim(No **lista, int num) {
+void inserir_no_fim(Lista *lista, int num) {
     No *aux, *novo = malloc(sizeof(No));
 
     if(novo) {
@@ -34,16 +45,17 @@ void inserir_no_fim(No **lista, int num) {
         novo -> proximo = NULL;
 
         // é o primeiro?
-        if(*lista == NULL){
-            *lista = novo;
+        if(lista -> inicio == NULL){
+            lista -> inicio = novo;
         }
         else{
-            aux = *lista;
+            aux = lista -> inicio;
             while(aux -> proximo){
                 aux = aux -> proximo;
             }
             aux -> proximo = novo;
         }
+        lista -> tam++;
     }
     else{
         printf("Erro ao alocar memoria!\n");
@@ -51,32 +63,62 @@ void inserir_no_fim(No **lista, int num) {
 }
 
 // Procedimento para inserir no meio
-void inserir_no_meio(No **lista, int num, int ant){
+void inserir_no_meio(Lista *lista, int num, int ant){
     No *aux, *novo = malloc(sizeof(No));
 
     if(novo){
         novo -> valor = num;
         // É o primeiro
-        if(*lista == NULL){
+        if(lista -> inicio == NULL){
             novo -> proximo = NULL;
-            *lista = novo;
+            lista -> inicio = novo;
         }
         else{
-            aux = *lista;
+            aux = lista -> inicio;
             while(aux -> valor != ant && aux -> proximo){
                 aux = aux -> proximo;
             }
             novo -> proximo = aux -> proximo;
             aux -> proximo = novo;
         }
+        lista -> tam++;
     }
     else{
         printf("Erro ao alocar memoria!\n");
     }
 }
 
-void imprimir (No *no){
-    printf("\n\tLista: ");
+void inserir_ordenado(Lista *lista, int num){
+    No *aux, *novo = malloc(sizeof(No));
+
+    if (novo) {
+        novo -> valor = num;
+        // A lista está vazia?
+        if(lista -> inicio == NULL){
+            novo -> proximo = NULL;
+            lista -> inicio = novo;
+        } // É o menor
+        else if(novo -> valor < lista -> inicio){
+            novo -> proximo = lista -> inicio;
+            lista -> inicio = novo;
+        }
+        else {
+            aux = lista -> inicio;
+            while(aux -> proximo && novo -> valor > aux -> proximo -> valor){
+                aux = aux -> proximo;
+            }
+            novo -> proximo = aux -> proximo;
+            aux -> proximo = novo;
+        }
+    }
+    else {
+        printf("Erro ao alocar memoria");
+    }
+}
+
+void imprimir (Lista lista){
+    No *no = lista.inicio;
+    printf("\n\tLista tam %d: ", lista.tam);
     while(no){
         printf("%d ", no->valor);
         no = no -> proximo;
@@ -86,10 +128,13 @@ void imprimir (No *no){
 int main(){
 
     int opcao, valor, anterior;
-    No *lista = NULL;
+    //No *lista = NULL;
+    Lista lista;
+
+    criar_lista(&lista);
 
     do{
-        printf("\n\t0 - Sair\n\t1 - InserirI\n\t2 - InserirF\n\t3 - InserirM\n\t4 - Imprimir\n\t");
+        printf("\n\t0 - Sair\n\t1 - InserirI\n\t2 - InserirF\n\t3 - InserirM\n\t4 - InserirO\n\t5 - Imprimir\n\t");
         scanf("%d", &opcao);
         
         switch(opcao){
@@ -109,6 +154,11 @@ int main(){
             inserir_no_meio(&lista, valor, anterior);
             break;
         case 4:
+            printf("Digite um valor e o valor de referencia: ");
+            scanf("%d%d", &valor, &anterior);
+            inserir_ordenado(&lista, valor);           
+            break;
+        case 5:
             imprimir(lista);
             break;
         default:
